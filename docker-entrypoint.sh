60a45501e3bb7fae62637789544d1264e4041589
ubergesundheit <ubergesundheit@googlemail.com>
2017-06-22 13:30:16 +0200

    make active machine stand out in PS1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
-echo "PS1='\[\e[1m\]\[\e[31m\]Active machine:\$(__docker_machine_ps1 \" %s\")\[\e[m\]\[\e[0m\]\n \[\e[1m\]\[\e[36m\]\w\[\e[m\] \[\e[32m\]\$\[\e[m\]\[\e[0m\] '" >> /home/user/.bashrc
+echo "PS1='Active machine:\[\e[1m\]\[\e[31m\]\$(__docker_machine_ps1 \" %s\")\[\e[m\]\[\e[0m\]\n \[\e[1m\]\[\e[36m\]\w\[\e[m\] \[\e[32m\]\$\[\e[m\]\[\e[0m\] '" >> /home/user/.bashrc
 
 chown -R user /workdir
 
