6d1eb01784cb28e54309d5e29d90ff12cdd56ac2
ubergesundheit <ubergesundheit@googlemail.com>
2017-07-10 16:25:43 +0200

    allow custom ssh_config file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
@@ -7,9 +9,15 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+useradd --shell /bin/bash -u "$USER_ID" -o -c "" -m user
 export HOME=/home/user
 echo "PS1='Active machine:\[\e[1m\]\[\e[31m\]\$(__docker_machine_ps1 \" %s\")\[\e[m\]\[\e[0m\]\n \[\e[1m\]\[\e[36m\]\w\[\e[m\] \[\e[32m\]\$\[\e[m\]\[\e[0m\] '" >> /home/user/.bashrc
+
+# symlink /workdir/ssh_config to /etc/ssh/ssh_config if it exits
+if [ -f /workdir/ssh_config ]; then
+  mv /etc/ssh/ssh_config /etc/ssh/ssh_config.bkp
+  ln -s /workdir/ssh_config /etc/ssh/ssh_config
+fi
 
 chown -R user /workdir
 
