2a0624614a9a7bb41ea57f26507285cc216f119f
umut <u.tas@reedu.de>
2020-09-07 13:12:47 +0200

    add blockdevice to mongo hosts because of openstack bug

--- infrastructure/openstack-opensensemap-terraform/mongo_new.tf
+++ infrastructure/openstack-opensensemap-terraform/mongo_new.tf
@@ -6,12 +6,23 @@ resource "openstack_compute_instance_v2" "mongo_host_tb" {
   flavor_id = "${var.mongo_flavor}"
   image_id  = "${var.ubuntu18_image_id}"
   key_pair  = "${var.openstack_osem_keypair}"
-  security_groups = ["${openstack_compute_secgroup_v2.mongo_internal_tb.name}", "${openstack_compute_secgroup_v2.ssh_from_bastion.name}", "${openstack_compute_secgroup_v2.prometheus_internal.name}"]
+  security_groups = ["${openstack_compute_secgroup_v2.mongo_internal_new.name}", "${openstack_compute_secgroup_v2.ssh_from_bastion.name}", "${openstack_compute_secgroup_v2.prometheus_internal.name}"]
 
   network {
     uuid = "${openstack_networking_subnet_v2.internal-subnet.network_id}"
     name = "osem-internal"
   }
+
+    block_device {
+      uuid                  = "${var.ubuntu18_image_id}"
+      source_type           = "image"
+      destination_type      = "volume"
+      boot_index            = 0
+      delete_on_termination = true
+      volume_size           = 12
+      volume_type           = "replicated_gold"
+    }
+
 }
 
 resource "openstack_blockstorage_volume_v2" "mongo_volume_tb" {
